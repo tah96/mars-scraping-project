e3c0faf3b6ccb4e5c306ce7f4b55904da8941582
tah96 <tylerandrewhunt96@gmail.com>
2021-01-26 22:46:04 -0500

    New bug. Chromedriver needs updating. Temp save

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+#Create Flask application and connect to MongoDB URL for connection
+
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,7 +3,12 @@ import requests
 from splinter import Browser
 import pandas as pd
 
+# Define scrape which will be used in FLASK app.py
+# Intent is to allow user to click a button to call this function
+
 def scrape():
+    #Define executable path so browser knows where to pull commands from
+
     executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
 
@@ -11,24 +16,25 @@ def scrape():
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
     html = browser.html
+
+    #BeautifulSoup to parse through HTML
+
     soup = bs(html, 'html.parser')
+
+    #Use soup to parse through HTML to find news snippets
 
     results = soup.find_all('div', class_="list_text")
     news_list = []
 
+    #For loop to grab title and summary paragraph from each snippet
+
     for result in results:
         try:
-            # Identify and return title of listing
             news_title = result.find('a').text
-            # Identify and return price of listing
             news_p = result.find('div', class_="article_teaser_body").text
 
-            # Print results only if title, price, and link are available
-            #if (news_title and news_p):
-                #print('-------------')
-                #print(f'Title: {news_title}')
-                #print('')
-                #print(f'Description: {news_p}')
+            #Dictionary included with intent of importing to mongoDB
+
             news_dict = {'News Title': news_title,
                         'News Description': news_p}
             news_list.append(news_dict)
@@ -36,7 +42,6 @@ def scrape():
         except AttributeError as e:
             print(e)
 
-    #REMEMBER YOU WILL LIKELY NEED TO CREATE DICTIONARY TO INPUT TO MONGODB
 
     url_jpl_base = 'https://www.jpl.nasa.gov'
     url_jpl_spaceimages_home_endpoint = '/spaceimages/?search=&category=Mars'
@@ -45,39 +50,35 @@ def scrape():
 
     html_jpl = browser.html
     soup_jpl = bs(html_jpl, 'html.parser')
-    #print(soup_jpl.prettify())
-
 
     featured_image_filter = soup_jpl.find('div', class_="default floating_text_area ms-layer")
-    #featured_image_filter
 
     featured_image_endpoint = featured_image_filter.footer.a['data-fancybox-href']
-    #featured_image_endpoint
 
     featured_image_url = url_jpl_base + featured_image_endpoint
-    #featured_image_url
 
     ## Mars Facts ##
 
     url_mars_facts_base = 'https://space-facts.com/mars/'
     browser.visit(url_mars_facts_base)
 
+    #Pandas used to read in HTML tables
+
     tables = pd.read_html(url_mars_facts_base)
 
-    #type(tables)
+    #First table in list provides information about Mars. Extract
 
     df = tables[0]
     df.columns = ['Description', 'Information']
-    #df.head()
 
+    #Set index and then remove name. Don't want description column name in final product
 
     df.set_index('Description', inplace=True)
     df.index.name = ''
-    #df.head()
 
+    #Convert dataframe to HTML
 
     html_table = df.to_html()
-    #html_table
 
     ## Mars Hemispheres ##
 
@@ -88,12 +89,12 @@ def scrape():
 
     html_mars_hemi = browser.html
     soup_mars_hemi = bs(html_mars_hemi, 'html.parser')
-    #print(soup_mars_hemi.prettify())
 
     mars_hemi_filter = soup_mars_hemi.find_all('div', class_="item")
-    #mars_hemi_filter
 
     hemisphere_image_urls = []
+
+    #Run for loop to capture image titles and URLs for each respective hemisphere
 
     for entry in mars_hemi_filter:
         image_endpoint = entry.a['href']
@@ -104,13 +105,14 @@ def scrape():
         image_src_filter = soup_entry.find('img', class_= 'wide-image')['src']
         image_src_filter_url = url_mars_hemi_base + image_src_filter
         image_title_filter = soup_entry.find('h2', class_= 'title').get_text()
+
+        #Create dictionary with intent to enter into mongoDB later
+
         image_dict = {'title': image_title_filter,
                     'img_url': image_src_filter_url}
         hemisphere_image_urls.append(image_dict)
-        #print(image_src_filter_url)
-        #print(image_title_filter)
 
-    #hemisphere_image_urls
+    #Create dictionary of arrays
 
     scraped_data = {
         'news_list' : news_list,
@@ -118,6 +120,8 @@ def scrape():
         'html_table': html_table,
         'hemisphere_image_urls': hemisphere_image_urls
     }
+
+    #Close browser
 
     browser.quit()
 
